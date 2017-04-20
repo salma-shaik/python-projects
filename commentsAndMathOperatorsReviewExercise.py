5513e68c21c491d1c0e5aae3d59069648617e8ab
Salma Shaik <salmashaik1611@gmail.com>
2017-04-20 18:52:51 +0530

    comments and math operators

---
+++ commentsAndMathOperatorsReviewExercise.py
@@ -0,0 +1,22 @@
+# This is a single line comment
+"""
+    This is
+    a multi line
+    comment
+"""
+
+#Math Operators
+add = 2+3
+sub = 4-2
+div = 8/2
+prod = 3*4
+
+#Assignment Operators
+operators1 = operators2 = operators3 = 2
+operators1 **= 2
+operators2 //= 0.5
+operators3 %= 2
+
+#Output
+print(add, sub, div, prod)
+print(operators1, operators2, operators3)
