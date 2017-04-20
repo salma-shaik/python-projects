466d21611fb20b22a9a3b7468f9c3a8117d294cc
Salma Shaik <salmashaik1611@gmail.com>
2017-04-20 19:07:42 +0530

    variables and datatypes demo

---
+++ variableAndDatatypesReviewExercise.py
@@ -0,0 +1,14 @@
+intVal = 39
+floatVal = 8343.8989
+boolVal = True
+print(boolVal)
+boolVal = False
+print(intVal)
+print(floatVal)
+print(boolVal)
+"""
+    this is a
+    multiline
+    comment
+"""
+#this is a single line comment
---
+++ variablesAndDataTypes.py
@@ -0,0 +1,7 @@
+variableNamesCannotBeginWithANumber = True
+initial = 3
+boolean = False
+posInt = 20
+zero = 0
+negFloat = -8.8989
+initial = 7
