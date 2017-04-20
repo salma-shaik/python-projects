ff8e883ff4478b13bdacb28ee80e8fc7934b15f0
Salma Shaik <salmashaik1611@gmail.com>
2017-04-20 18:52:11 +0530

    built-in functions demo

---
+++ builtInFunctions.py
@@ -0,0 +1,32 @@
+"""
+abs() and type():
+1.use abs() to get and print the absolute value of a positive number
+2.use abs() to get and print the absolute value of a negative number
+3.use type() to get and print the type of a float
+4.use type() to get and print the type of an integer
+5.use type() to get and print the type of a boolean value
+6.use type() to get and print the type of a string
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+
+print(abs(5))
+print(abs(-4))
+
+print(type(4.32))
+print(type(3))
+print(type(True))
+print(type("Test"))
+# ----------------------------------------------------------------------------------------------------------------------
+"""
+max() and min():
+1.use max() to get and print the largest of inputs of your choosing that are all integers
+2.use max() to get and print the alphabetical maximum of inputs of your choosing that are all strings
+3.use min() to get and print the smallest of inputs of your choosing that are all floats
+4.use min() to get and print the alphabetical minimum of inputs of your choosing that are all strings
+"""
+# ----------------------------------------------------------------------------------------------------------------------
+print(max(2,4,1232,532,3632,24,45))
+print(max("dad","rwrw","wqe","ewrew"))
+print(min(3.22,3.23,5.23432,2323.5))
+print(min("fsd","sds","wqs","adasd"))
+# ----------------------------------------------------------------------------------------------------------------------
