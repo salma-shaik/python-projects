6addb8f4dde6cab17acffc192498c07fddd7501f
Salma Shaik <salmashaik1611@gmail.com>
2018-05-25 09:44:37 -0500

    importing modules; leap year; find month days

---
+++ yt_pb71_cs/import_index_search_ex.py
@@ -0,0 +1,19 @@
+# #import index_search as in_s
+#
+# import sys
+#
+# sys.path.append('/Users/salma/Studies/Python/python-projects/yt_pb71_cs/module')
+#
+# from index_search import find_index, test
+
+#import module.index_search as in_s
+
+from module.index_search import find_index
+courses = ['Art', 'Math', 'Science', 'History']
+
+print(find_index(courses, 'Science'))
+
+# print(test)
+#
+# print(sys.path)
+
