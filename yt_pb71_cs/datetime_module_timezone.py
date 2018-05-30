b536a0d7e7ed7d93e1556840c079664a7ec3297c
Arvind <europa1613@users.noreply.github.com>
2018-05-30 12:02:30 -0500

    date to string conversion and viceversa

--- yt_pb71_cs/datetime_module_timezone.py
+++ yt_pb71_cs/datetime_module_timezone.py
@@ -17,6 +17,8 @@ datetime1 = datetime.datetime(2016, 7, 27, 12, 30, 45, tzinfo=pytz.UTC)
 print(datetime1)
 
 datetime1_now = datetime.datetime.now(tz=pytz.UTC)
+
+'''
 print('datetime1_now in UTC: ', datetime1_now)
 
 dt_mtn = datetime1_now.astimezone(pytz.timezone('US/Eastern'))
@@ -24,4 +26,11 @@ print(dt_mtn)
 
 for tz in pytz.all_timezones:
     print(tz)
+'''
 
+# strftime - converts datetime to a string
+date_str = datetime1_now.strftime('%B %d, %Y')
+print('Formatted date time: ', date_str)
+
+# strptime - converts string to a datetime
+print('Date time from date string: ', datetime.datetime.strptime(date_str, '%B %d, %Y'))
