a2b9a03b49274567b307aba0e4a99ac2e76bf9fe
Salma Shaik <salmashaik1611@gmail.com>
2018-06-01 13:41:23 -0500

    understanding __name__ = __main__

---
+++ yt_pb71_cs/name_main_first.py
@@ -0,0 +1,14 @@
+# if __name__ == '__main__':
+#     print("Run directly")
+# else:
+#     print("Run from import")
+
+print("This will always run")
+
+
+def main():
+    print("First module's main method")
+
+
+if __name__ == '__main__':
+    main()
---
+++ yt_pb71_cs/name_main_second.py
@@ -0,0 +1,4 @@
+import name_main_first
+
+name_main_first.main()
+print(f"Second module's name: {__name__}")
